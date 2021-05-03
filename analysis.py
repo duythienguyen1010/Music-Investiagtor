025387d06edc767e2e47ed1a940e17cbbff83056
Hoang Nguyen <duythienguyen1010@gmail.com>
2021-05-03 17:17:45 -0400

    add another function, change in tesing file

--- analysis.py
+++ analysis.py
@@ -2,6 +2,7 @@ import pandas as pd
 import plotly.graph_objs as go
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import datetime
 
 def generate_elements( x = ''):
     # Spotipy Setup
@@ -16,7 +17,7 @@ def generate_elements( x = ''):
     for i, t in enumerate(playlists['items']):
         playlistIDs.append(t['id'])
 
-    # Tracks in First Playlist
+    # Tracks in Playlist
     tracks = sp.playlist_items(playlist_id=playlistIDs[0], limit=100, offset=0)
     trackIDs = []
     total = tracks['total']
@@ -54,7 +55,6 @@ def generate_elements( x = ''):
     sum_live = sum(liveness) / total
     sum_val = sum(valence) / total
 
-    print(sum_danc, sum_acou, sum_ene, sum_inst, sum_speech, sum_live, sum_val)
     taste = ['danceability', 'acousticness', 'energy', 'instrumentalness',
              'speechiness', 'liveness', 'valence']
     scale = [sum_danc, sum_acou, sum_ene, sum_inst, sum_speech, sum_val]
@@ -77,4 +77,64 @@ def star_graph(taste = [], scale = []):
     )
     return fig
 
-#generate_elements('mh6sdur1otqgq0rhbllwa8k5f')
+def generate_general_taste (country= ''):
+    # Spotipy Setup
+    cid = '3e7b0a3fcfe445a69eea02e4a4ce99b8'
+    secret = 'a761b0fa42734f5aa5a4182f425558c6'
+    client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+    # generate timestamp
+    ct = datetime.datetime.utcnow().isoformat()
+
+    # country
+    country = 'US'
+
+    # Playlists
+    playlistIDs = []
+    for i in range(0, 1000, 50):
+        playlists = sp.featured_playlists(country=country, timestamp=ct, limit=50)
+        for i, t in enumerate(playlists['playlists']['items']):
+            playlistIDs.append(t['id'])
+
+    # Tracks in Playlist
+    tracks = sp.playlist_items(playlist_id=playlistIDs[0], limit=100, offset=0)
+    trackIDs = []
+    total = tracks['total']
+    for i, t in enumerate(tracks['items']):
+        trackIDs.append(t['track']['id'])
+
+    # Features
+    danceability = []
+    acousticness = []
+    energy = []
+    instrumentalness = []
+    loudness = []
+    speechiness = []
+    liveness = []
+    valence = []
+    tempo = []
+
+    for i in range(0, len(trackIDs)):
+        features = sp.audio_features(trackIDs[i])
+        danceability.append(features[0]['danceability'])
+        acousticness.append(features[0]['acousticness'])
+        energy.append(features[0]['energy'])
+        instrumentalness.append(features[0]['instrumentalness'])
+        speechiness.append(features[0]['speechiness'])
+        liveness.append(features[0]['liveness'])
+        valence.append(features[0]['valence'])
+
+    # Calculate average features
+    sum_danc = sum(danceability) / total
+    sum_acou = sum(acousticness) / total
+    sum_ene = sum(energy) / total
+    sum_inst = sum(instrumentalness) / total
+    sum_speech = sum(speechiness) / total
+    sum_live = sum(liveness) / total
+    sum_val = sum(valence) / total
+
+    taste = ['danceability', 'acousticness', 'energy', 'instrumentalness',
+             'speechiness', 'liveness', 'valence']
+    scale = [sum_danc, sum_acou, sum_ene, sum_inst, sum_speech, sum_val]
+    return taste, scale
